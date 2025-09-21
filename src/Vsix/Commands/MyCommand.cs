7a3c299860c719ab458a1cbfaa0d18b767fe7a24
Erik Ejlskov Jensen <eej@delegate.dk>
2025-09-21 15:54:06 +0200

    Initial

---
+++ src/Vsix/Commands/MyCommand.cs
@@ -0,0 +1,11 @@
+﻿namespace SqlProjectsPowerTools
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            await VS.MessageBox.ShowWarningAsync("SQL Database Projects Power Tools", "Button clicked");
+        }
+    }
+}
---
+++ src/Vsix/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(SqlProjectsPowerTools.Vsix.Name)]
+[assembly: AssemblyDescription(SqlProjectsPowerTools.Vsix.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(SqlProjectsPowerTools.Vsix.Author)]
+[assembly: AssemblyProduct(SqlProjectsPowerTools.Vsix.Name)]
+[assembly: AssemblyCopyright(SqlProjectsPowerTools.Vsix.Author)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion(SqlProjectsPowerTools.Vsix.Version)]
+[assembly: AssemblyFileVersion(SqlProjectsPowerTools.Vsix.Version)]
+
+namespace System.Runtime.CompilerServices
+{
+    public class IsExternalInit { }
+}
