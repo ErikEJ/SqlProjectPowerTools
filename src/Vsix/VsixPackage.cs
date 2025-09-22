ed220d54e6fd31dca350c65940e64f472ee5a970
Erik Ejlskov Jensen <ejlskov@hotmail.com>
2025-09-22 13:48:19 +0200

    fixes #4 fixes #5

--- src/Vsix/VsixPackage.cs
+++ src/Vsix/VsixPackage.cs
@@ -11,8 +11,19 @@ namespace SqlProjectsPowerTools
     [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(PackageGuids.VsixString)]
+    [ProvideAutoLoad(UIContextGuid, PackageAutoLoadFlags.BackgroundLoad)]
+    [ProvideUIContextRule(
+        UIContextGuid,
+        name: "Auto load based on rules",
+        expression: "SdkProject | SqlprojProject ",
+        termNames: ["SdkProject", "SqlprojProject"],
+        termValues: [$"ActiveProjectCapability:{SdkProjCapability}", "ActiveProjectBuildProperty:DSP=.*"])]
+
     public sealed class VsixPackage : ToolkitPackage
     {
+        public const string UIContextGuid = "E098D400-A841-4C88-9B7C-267EFA15A5E4";
+        public const string SdkProjCapability = "MSBuild.Sdk.SqlProj.BuildTSqlScript";
+
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
         {
             await this.RegisterCommandsAsync();
