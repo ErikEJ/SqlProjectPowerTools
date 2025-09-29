7dd7ad9f73d0f902cbf865203c36e9367a842881
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2025-09-29 10:52:45 +0200

    Make tool zip smaller (#52)

--- src/DacFXTool/BuildCmdlineTool.cmd
+++ src/DacFXTool/BuildCmdlineTool.cmd
@@ -8,6 +8,20 @@ rd bin\Release\net8.0\x64\publish\runtimes\unix /Q /S
 rd bin\Release\net8.0\x64\publish\runtimes\win-arm /Q /S
 rd bin\Release\net8.0\x64\publish\runtimes\win-x86 /Q /S
 
+rd bin\Release\net8.0\x64\publish\cs /Q /S
+rd bin\Release\net8.0\x64\publish\de /Q /S
+rd bin\Release\net8.0\x64\publish\es /Q /S
+rd bin\Release\net8.0\x64\publish\fr /Q /S
+rd bin\Release\net8.0\x64\publish\it /Q /S
+rd bin\Release\net8.0\x64\publish\ja /Q /S
+rd bin\Release\net8.0\x64\publish\ko /Q /S
+rd bin\Release\net8.0\x64\publish\pl /Q /S
+rd bin\Release\net8.0\x64\publish\pt-BR /Q /S
+rd bin\Release\net8.0\x64\publish\ru /Q /S
+rd bin\Release\net8.0\x64\publish\tr /Q /S
+rd bin\Release\net8.0\x64\publish\zh-Hans /Q /S
+rd bin\Release\net8.0\x64\publish\zh-Hant /Q /S
+
 "C:\Program Files\7-Zip\7z.exe" -mm=Deflate -mfb=258 -mpass=15 a dacfxtool.exe.zip .\bin\Release\net8.0\x64\publish\*
 
 move /Y dacfxtool.exe.zip ..\lib\
