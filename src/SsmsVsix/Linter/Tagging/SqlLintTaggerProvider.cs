f3ab25cafa3bb96a90b83176fee09b20c23dd3f3
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2026-03-27 10:57:34 -0700

    Make the update check work (#265)
    * Make the update check work
    
    * Exit early

--- src/SsmsVsix/Linter/Tagging/SqlLintTaggerProvider.cs
+++ src/SsmsVsix/Linter/Tagging/SqlLintTaggerProvider.cs
@@ -3,7 +3,6 @@ using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Editor;
 using Microsoft.VisualStudio.Text.Tagging;
 using Microsoft.VisualStudio.Utilities;
-using SqlProjectsPowerTools;
 using SqlProjectsPowerTools.Linting;
 
 namespace SqlProjectsPowerTools.Tagging
@@ -29,7 +28,8 @@ namespace SqlProjectsPowerTools.Tagging
                 return null;
             }
 
-            if (ToolOptions.Instance.DisableCodeAnalysis)
+            if (buffer.Properties.TryGetProperty(typeof(ITextDocument), out ITextDocument document)
+                && (!document.FilePath?.EndsWith(".sql", StringComparison.OrdinalIgnoreCase) ?? true))
             {
                 return null;
             }
@@ -37,17 +37,23 @@ namespace SqlProjectsPowerTools.Tagging
             var enabled = false;
             var sqlVersion = string.Empty;
             var rules = string.Empty;
-            var project = string.Empty;
+            var projectName = string.Empty;
 
             ThreadHelper.JoinableTaskFactory.Run(async () =>
             {
                 await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
 
+                var options = await ToolOptions.GetLiveInstanceAsync();
+                if (options.DisableCodeAnalysis)
+                {
+                    return;
+                }
+
                 var currentProject = await VS.Solutions.GetActiveProjectAsync();
 
                 if (currentProject != null)
                 {
-                    project = currentProject.Name;
+                    projectName = currentProject.Name;
                     var runProperties = await currentProject.IsInSqlProjAsync();
                     enabled = runProperties.Run;
                     sqlVersion = runProperties.SqlVersion;
@@ -60,15 +66,9 @@ namespace SqlProjectsPowerTools.Tagging
                 return null;
             }
 
-            if (buffer.Properties.TryGetProperty(typeof(ITextDocument), out ITextDocument document)
-                && (!document.FilePath?.EndsWith(".sql", StringComparison.OrdinalIgnoreCase) ?? true))
-            {
-                return null;
-            }
-
             return buffer.Properties.GetOrCreateSingletonProperty(
                 typeof(SqlLintTagger),
-                () => new SqlLintTagger(buffer, AnalysisCache, sqlVersion, rules, project)) as ITagger<T>;
+                () => new SqlLintTagger(buffer, AnalysisCache, sqlVersion, rules, projectName)) as ITagger<T>;
         }
     }
 }
