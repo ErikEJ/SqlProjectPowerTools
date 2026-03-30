66a96a0b0c544555123c036c18f0cc454dda425c
Erik Ejlskov Jensen <ErikEJ@users.noreply.github.com>
2026-03-30 11:07:58 +0200

    More features for SSMS (#273)

--- src/Shared/Commands/DeployScriptCommand.cs
+++ src/Shared/Commands/DeployScriptCommand.cs
@@ -7,7 +7,7 @@ namespace SqlProjectsPowerTools
         {
             ThreadHelper.JoinableTaskFactory.Run(async () =>
             {
-                Command.Enabled = await Command.IsEnabledForMsBuildSdkSqlProjectAsync();
+                Command.Enabled = await Command.IsEnabledForModernSqlProjectAsync();
             });
         }
 
